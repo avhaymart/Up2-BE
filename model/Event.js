4f5248869ad17669325329f405464b0dc2ab8c43
Trent Cooper <Coop741@MacBook-Pro-41.local>
2019-02-22 12:28:01 -0700

    added initial event model and route

--- model/Event.js
+++ model/Event.js
@@ -1,2 +1,17 @@
-// Event model creation goes here
-// ==============================
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const eventSchema = new Schema({
+  first_name: String,
+  last_name: String,
+  date: Date,
+  title: String,
+  description: String,
+  image: String,
+  value: Number,
+  hashtag: String
+})
+
+const Event = mongoose.model('event', eventSchema);
+
+module.exports = Event;
